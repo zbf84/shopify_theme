e482564cc2916e0b769f68aff4dece14775ebb8c
torcop <animesh.ruet@gmail.com>
2025-01-21 16:39:46 +0600

    21-1-2025

---
+++ tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./layout/*.liquid', './sections/**/*.liquid', './templates/**/*.liquid'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
